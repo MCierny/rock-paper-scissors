fc4d6d9a2582b52e71af397d40e3cf50bff6c6d1
MCierny <122530624+MCierny@users.noreply.github.com>
2023-02-02 20:19:51 +0100

    Create new function game()
    Added incrementation on playerScore and comoputerScore
    to gameRound() based on the outcome. To keep track of score
    Used for loop to play the round 5 times.
    After 5 rounds, declare a winner using alert.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-
-
 function getComputerChoice() {
     const compChoice = Math.floor(Math.random() * 3);
     if (compChoice === 0) {
@@ -33,31 +31,72 @@ else if player == paper and computer == paper, print "its a TIE"
 else print "Wrong input, you probably made a spelling mistake"
 */
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("What is your choice? (Rock, Paper or Scissors").toLowerCase();
 
-function gameRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection === "Rock") {
-        return "Its a TIE!";
-    }   else if (playerSelection == "rock" && computerSelection === "Scissors") {
+function gameRound() {
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt("What is your choice? (Rock, Paper or Scissors)")?.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "Rock") {
+        return "Its a TIE! Rock vs Rock";
+    }   else if (playerSelection === "rock" && computerSelection === "Scissors") {
+            playerScore = ++playerScore;
             return "You WIN! Rock beats Scissors.";
-    }   else if (playerSelection == "rock" && computerSelection === "Paper") {
+    }   else if (playerSelection === "rock" && computerSelection === "Paper") {
+            computerScore = ++computerScore;
             return "You LOOSE! Paper beats Rock.";
-    }   else if (playerSelection == "scissors" && computerSelection === "Rock") {
+    }   else if (playerSelection === "scissors" && computerSelection === "Rock") {
+            computerScore = ++computerScore;  
             return "You LOOSE! Rock beats Scissors.";
-    }   else if (playerSelection == "scissors" && computerSelection === "Scissors") {
-            return "Its a TIE!";
-    }   else if (playerSelection == "scissors" && computerSelection === "Paper") {
+    }   else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+            return "Its a TIE! Scissors vs Scissors";
+    }   else if (playerSelection === "scissors" && computerSelection === "Paper") {
+            playerScore = ++playerScore;  
             return "You WIN! Scissors beat Paper.";
-    }   else if (playerSelection == "paper" && computerSelection === "Rock") {
-        return "You WIN! Paper beat Rock.";
-    }   else if (playerSelection == "paper" && computerSelection === "Scissors") {
-        return "You LOOSE! Scissors beat Paper.";
-    }   else if (playerSelection == "paper" && computerSelection === "Paper") {
-        return "Its a TIE!";
+    }   else if (playerSelection === "paper" && computerSelection === "Rock") {
+            playerScore = ++playerScore;   
+            return "You WIN! Paper beats Rock.";
+    }   else if (playerSelection === "paper" && computerSelection === "Scissors") {
+            computerScore = ++computerScore; 
+            return "You LOOSE! Scissors beat Paper";
+    }   else if (playerSelection === "paper" && computerSelection === "Paper") {
+            return "Its a TIE Paper vs Paper.";
     }   else {
-        return "Wrong input, you probably made a spelling mistake";
+            return "Wrong input, you probably made a spelling mistake!";
     }
 }
 
-console.log(gameRound(playerSelection, computerSelection));
+/* Crate a function that will play 5 rounds and keps score, and reports winner oor loser at the end */
+
+/* 
+Create variable playerScore
+Create variable computerScore
+Create for loop with function gameRound()
+if player wins round +1 score
+if computer wins round +1 score
+if player score = 3 first = Player WINS
+if computer score = 3 first = Computer WINS
+*/
+
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(gameRound());
+        console.log("Your score: ", playerScore);
+        console.log("Opponent score: ", computerScore);
+    }  
+    if (playerScore > computerScore) {
+        alert("You WON!");
+        console.log("You WON the GAME!");
+    } else if (playerScore < computerScore) {
+        alert("You LOST!");
+        console.log("You LOST the GAME!");
+    } else if (playerScore === computerScore) {
+        alert("Its a TIE!")
+        console.log("Its a TIE!");
+    }
+}
+
+game();
+
+
